c6da6c42709415ec6ad96def3032b3ff3a7e329d
Vanshika Shukla <67906681+Vanshika0729@users.noreply.github.com>
2021-04-29 15:22:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,17 @@
             echo "<p>Responses</p>";
         ?>
         <a href="Register.php"> Click here to Register
+		<form action="register.php" method="POST">
+           Enter First_name: <input type="text" 
+           name="First_name" required="required" /> <br/>
+           Enter Second_name: <input type="text" 
+           name="Second_name" required="required" /> <br/>
+           Enter Email-ID: <input type="text" 
+           name="Email-ID" required="required" /> <br/>
+		   Enter Subject: <input type="text" 
+           name="Subject" required="required" /> <br/>
+		   Enter Message: <input type="text" 
+           name="Message" required="required" /> <br/>
+        </form>
     </body>
 </html> 
