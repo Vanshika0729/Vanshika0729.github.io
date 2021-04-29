31b31c905612df0720e75dfb32f92c03a5eaa873
Vanshika Shukla <67906681+Vanshika0729@users.noreply.github.com>
2021-04-29 15:07:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Response Page</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Responses</p>";
+        ?>
+        <a href="Register.php"> Click here to Register
+    </body>
+</html> 
